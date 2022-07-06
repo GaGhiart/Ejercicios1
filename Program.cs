3745a171c85b7e04c53a231f6892f5fa11f47296
GaGhiart <gabrielghiart@gmail.com>
2022-07-06 15:58:34 -0300

    Agrega el segundo ejercicio que piden en el curso (Pintar hora actual)

--- Program.cs
+++ Program.cs
@@ -3,3 +3,7 @@ string nombre = Console.ReadLine();
 Console.WriteLine("Escribe tu edad");
 string edad = Console.ReadLine();
 Console.WriteLine("Tu nombre es " + nombre + "y tu edad es " + edad);
+
+
+string hora = DateTime.Now.ToString("hh:mm:ss tt");
+Console.WriteLine("Hora actual " + hora);
