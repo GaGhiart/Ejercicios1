ae68f98dcdf163400b2ca8518914b167b14578e1
GaGhiart <gabrielghiart@gmail.com>
2022-07-07 14:38:29 -0300

    Agreguego 2do programa

---
+++ Operadores de Asignacion.cs
@@ -0,0 +1,30 @@
+﻿//operadores
+// Aritmeticos + - * / % ++ --
+/*
+int i = 4;
+//Console.WriteLine("Suma: "+ (i + 1));
+//Console.WriteLine("Division: " + i / 4);
+/* AUTO INCREMENTOS
+ * 
+Console.WriteLine("AutoIncremento: " + ++i);
+Console.WriteLine("AutoDecremento: " + --i);
+Console.WriteLine("Valor: " + i);
+*/
+/*
+Console.WriteLine("Division: " + i);
+Console.WriteLine("Division: " + i / 4);
+*/
+//Operadores de asignacion 
+/*
+int j = 1;
+//j = j + 3;
+Console.WriteLine("j: " + j);
+Console.WriteLine("Sumo 3: " + ( j+=3));
+*/
+
+int j = 1;
+int k = 1;
+
+Console.WriteLine("Iguales? " + (j == k));
+Console.WriteLine("Mayor" + (j > k));
+Console.WriteLine("Menor" + (j < k));
