fe65303eee3af1b589278300c44c4f93b5fdcc7c
GaGhiart <gabrielghiart@gmail.com>
2022-07-07 14:00:16 -0300

    Agrega Nullables

--- Program.cs
+++ Program.cs
@@ -1,21 +1,30 @@
 ﻿/*
- * 
- Console.WriteLine("Escribe tu nombre");
+Console.WriteLine("Escribe tu nombre");
 string nombre = Console.ReadLine();
 Console.WriteLine("Escribe tu edad");
-string edad = Console.ReadLine();
-Console.WriteLine("Tu nombre es " + nombre + "y tu edad es " + edad);
+
+int edad = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Escribe tu ciudad");
+string ciudad = Console.ReadLine();
+
+
+Console.WriteLine("Tu nombre es " + nombre);
+Console.WriteLine("Tu edad es " + edad);
+Console.WriteLine("Tu ciudad es " + ciudad);
+
 
 
 string hora = DateTime.Now.ToString("hh:mm:ss tt");
 Console.WriteLine("Hora actual " + hora );
 
-*/
+
 
 
 //Tipos de datos
+*/
 /*
-int i = 0;
+int?  i = null;
 long l = 1;
 
 float f = 1.55555f;
@@ -33,5 +42,30 @@ Console.WriteLine("  double: " + d);
 Console.Write("char: " + c);
 Console.WriteLine(" string: " + str);
 
+/*
+
+//Convertir string a Int
+/*
+string str2 = "32";
+
+  int miInt2 = Convert.ToInt32(str2);
+
+miInt2 = miInt2 + 1;
+
+Console.Write("str2: " + str2);
+Console.WriteLine(" miFloat: " + miInt2);
+
+const float pi = 3.1415f; //constante matematica
+
+
+//codigo
+float cualquierFloat = pi * 2;
+//
 */
 
+//nullable
+
+int? i = null;
+//
+
+Console.WriteLine(i + 1);
