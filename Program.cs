cbca30a36dfdd5fe9c921aa2fbcae90721f54983
GaGhiart <gabrielghiart@gmail.com>
2022-07-07 11:03:47 -0300

    Agrega tipo de datos disponibles en C#

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿Console.WriteLine("Escribe tu nombre");
+﻿/*
+ * 
+ Console.WriteLine("Escribe tu nombre");
 string nombre = Console.ReadLine();
 Console.WriteLine("Escribe tu edad");
 string edad = Console.ReadLine();
@@ -6,4 +8,30 @@ Console.WriteLine("Tu nombre es " + nombre + "y tu edad es " + edad);
 
 
 string hora = DateTime.Now.ToString("hh:mm:ss tt");
-Console.WriteLine("Hora actual " + hora);
+Console.WriteLine("Hora actual " + hora );
+
+*/
+
+
+//Tipos de datos
+/*
+int i = 0;
+long l = 1;
+
+float f = 1.55555f;
+double d = 2.6666666d;
+
+char c = 'c';
+string str = "String";
+
+Console.Write("int: " + i);
+Console.WriteLine(" long: " + l);
+
+Console.Write("float: " + f);
+Console.WriteLine("  double: " + d);
+
+Console.Write("char: " + c);
+Console.WriteLine(" string: " + str);
+
+*/
+
